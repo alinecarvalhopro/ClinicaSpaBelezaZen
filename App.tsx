28ac54bbee7de1951a027ac8ff9af4a88ee59314
aline <alinecarvalho.pro@gmail.com>
2023-11-14 14:14:21 -0300

    feat: initial project settings

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Clínica e SPA Beleza Zen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ src/Styles/Theme/Colors/Colors.ts
@@ -0,0 +1,10 @@
+export const Colors = {
+  white: '#FFFFFF',
+  grey100: '#545454',
+  grey50: '#ACACAC',
+  blue: '#483083',
+  green100: '#A3E0DA',
+  green60: '#D6F1F0',
+  green30: '#D6F1F094',
+  red: '#FF0000',
+};
